ec3165f3c59a4ca76b9621b053001e1f130e3e86
SCFrancisco2221857 <2221857@my.ipleiria.pt>
2025-08-01 14:41:21 +0100

    Initial commit

---
+++ src/test/java/com/example/witchallenge/WitChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.witchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
